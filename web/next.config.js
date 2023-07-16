8259699b173292c6b2c0b027aa58fc6887e106c6
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-17 03:19:49 +0900

    add image domains

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@motions/char"],
   experimental: {},
+
+  // img
+  images: {
+    domains: ["dev-dmt-out.s3.us-west-1.amazonaws.com"],
+  },
 };
 
 module.exports = nextConfig;
