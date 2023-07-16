c49a29720d3494da3ac00d8078032adbcc3df2bb
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-17 00:04:11 +0900

    init crud

--- services/service/src/app.module.ts
+++ services/service/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FilesModule } from './files/files.module';
+import { SessionsModule } from './sessions/sessions.module';
+import { TemplatesModule } from './templates/templates.module';
 
 @Module({
-  imports: [],
+  imports: [TemplatesModule, SessionsModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ services/service/src/templates/templates.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TemplatesController } from './templates.controller';
+import { TemplatesService } from './templates.service';
+
+describe('TemplatesController', () => {
+  let controller: TemplatesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TemplatesController],
+      providers: [TemplatesService],
+    }).compile();
+
+    controller = module.get<TemplatesController>(TemplatesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
