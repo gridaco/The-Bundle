13396e55563cbb0524e978fe098dd244221afcaa
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-19 16:15:08 +0900

    add GA

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import Script from "next/script";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6T6Q9XKY8S"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6T6Q9XKY8S');
+        `}
+      </Script>
+      <script></script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
