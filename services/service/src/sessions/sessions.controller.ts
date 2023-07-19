2b062c8ebc2e7db8d2775fe67818263dc83e000c
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-19 15:18:18 +0900

    fmt

--- services/service/src/sessions/sessions.controller.ts
+++ services/service/src/sessions/sessions.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SessionsService } from './sessions.service';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
