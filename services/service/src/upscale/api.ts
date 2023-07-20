9d5f443b9ce2e0c08af806a887c4f43c2c813e36
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-21 02:49:31 +0900

    add upscale api

---
+++ services/service/src/upscale/api.ts
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import FormData from 'form-data';
+import fs from 'fs';
+
+/**
+ * Response when requested with application/json
+ */
+interface UpscaleApiJsonResponse {
+  artifacts: ReadonlyArray<UpscaleApiArtifact>;
+}
+
+interface UpscaleApiArtifact {
+  /**
+   * Image encoded in base64
+   */
+  base64: string;
+  /**
+   * The seed associated with this image
+   */
+  seed: number;
+  finishReason: 'CONTENT_FILTERED' | 'ERROR' | 'SUCCESS';
+}
+
+/**
+ * Upscales an image 2x using the ESRGAN model. - uses api.stability.ai
+ * upscale2x('path-to-image', 'path-to-output')
+ *
+ * requires API key
+ *
+ * The api comes with two response modes, application/json and image/png. - this function only supports application/json
+ *
+ * @param input path to input image
+ * @param save path to save image - if not provided, will not write to disk
+ */
+export async function upscale2x(
+  input: string,
+  save?: string,
+): Promise<
+  UpscaleApiArtifact & {
+    save?: string;
+  }
+> {
+  // NOTE: This example is using a NodeJS FormData library.
+  // Browsers should use their native FormData class.
+  // React Native apps should also use their native FormData class.
+  const formData = new FormData();
+  formData.append('image', fs.createReadStream(input));
+
+  const config = {
+    headers: {
+      ...formData.getHeaders(),
+      Accept: 'application/json',
+      Authorization: 'Bearer YOUR_API_KEY',
+    },
+  };
+
+  const response = await axios.post<UpscaleApiJsonResponse>(
+    'https://api.stability.ai/v1/generation/esrgan-v1-x2plus/image-to-image/upscale',
+    formData,
+    config,
+  );
+
+  const result = response.data.artifacts[0];
+
+  if (save) {
+    fs.writeFileSync(save, Buffer.from(result.base64, 'base64'));
+    return {
+      ...result,
+      save: save,
+    };
+  }
+
+  return result;
+}
