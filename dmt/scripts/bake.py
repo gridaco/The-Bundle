b2ef6401b1670a16bb7c3feb8ac8e7fbb0cbe963
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-24 19:20:49 +0900

    add skip & res

--- dmt/scripts/bake.py
+++ dmt/scripts/bake.py
@@ -1,4 +1,5 @@
 import math
+from pathlib import Path
 import bpy
 import click
 from PIL import Image
@@ -9,7 +10,9 @@ from mathutils import Euler
 @click.option('--file', help='Path to .blend file', required=True, type=click.Path(exists=True, dir_okay=False, file_okay=True, resolve_path=True))
 @click.option('--out', help='Output folder', required=True, type=click.Path(exists=True, dir_okay=True, file_okay=False, resolve_path=True))
 @click.option('--step', help='Steps in degrees', type=int, default=6)
-def bake(file, out, step):
+@click.option('--skip-if-exists', help='Skip render for point if exists', is_flag=True)
+@click.option('--resolution-percentage', help='sets resolution_percentage', type=int, default=100)
+def bake(file, out, step, skip_if_exists, resolution_percentage):
 
     # Range of angles to rotate the object through
     # range function does not include the stop value, hence 46
@@ -36,6 +39,10 @@ def bake(file, out, step):
 
     # Iterate over each angle in each range
     for x, y, z in points:
+        outfile = f'{out}/render_x{x}_y{y}_z{z}.png'
+        if skip_if_exists and Path(outfile).exists():
+            continue
+
         print(f'x: {x}, y: {y}, z: {z}')
         # Rotate object
         obj.rotation_euler = \
@@ -44,10 +51,10 @@ def bake(file, out, step):
         print('ROTATION', obj.rotation_euler)
 
         # Define render output path
-        scene.render.filepath = f'{out}/render_x{x}_y{y}_z{z}.png'
+        scene.render.filepath = outfile
 
         # Down resolution % for testing
-        scene.render.resolution_percentage = 25
+        scene.render.resolution_percentage = resolution_percentage
 
         # Render scene
         bpy.ops.render.render(write_still=True)
