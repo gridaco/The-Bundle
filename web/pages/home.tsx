b5faa79ff3c5e74d754724a27b705e6f297c30f5
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-30 18:31:42 +0900

    connect upgrade button w/modal

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -13,7 +13,6 @@ export default function Home() {
   return (
     <main>
       <TemplateDropdown />
-      <UpgradeToProBadge />
       <UpgradeToProDialog />
     </main>
   );
@@ -25,7 +24,7 @@ function UpgradeToProDialog() {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className="Button violet">Upgrade</button>
+        <UpgradeToProBadge />
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="DialogOverlay" />
@@ -53,9 +52,9 @@ function UpgradeToProDialog() {
                 <>
                   <h1>Upgrade to Pro</h1>
                   <p>
-                    You’ve reached daily quota limit for Free tier limit.
                     Upgrade to Pro and get access to exclusive templates and
                     xxx.
+                    {/* TODO: update text */}
                   </p>
                   <button
                     onClick={() => {
--- web/scaffold/upgrade/upgrade-badge.tsx
+++ web/scaffold/upgrade/upgrade-badge.tsx
@@ -1,10 +1,27 @@
 import { DiamondIcon } from "@/icons/diamond";
+import styled from "@emotion/styled";
+import React from "react";
 
-export function UpgradeToProBadge() {
+export const UpgradeToProBadge = React.forwardRef(function UpgradeToProBadge(
+  props: React.ComponentProps<"button">,
+  ref: React.Ref<HTMLButtonElement>
+) {
   return (
-    <div>
+    <Badge ref={ref} {...props}>
       <DiamondIcon color="white" width={24} height={24} />
       Upgrade
-    </div>
+    </Badge>
   );
-}
+});
+
+const Badge = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 20px;
+
+  background: rgba(255, 255, 255, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 8px;
+`;
