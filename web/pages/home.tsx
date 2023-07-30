99ec408ff82891c23508c9b5f4a832e0ff9f3fef
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-30 18:18:34 +0900

    set modal routes

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -1,37 +1,104 @@
+import React from "react";
 import { ContinueWithGoogleButton } from "@/components/continue-with-google-button";
 import {
-  UpgradeModalLayout,
+  UpgradeToProSplashView,
   ColumnImages,
-} from "@/layouts/upgrade-modal-layout";
+  UpgradeToProPlansView,
+} from "@/scaffold/upgrade";
 import { TemplateDropdown } from "@/scaffold/home";
-import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
     <main>
       <TemplateDropdown />
-      <UpgradeModalLayout
-        hero={
-          <ColumnImages
-            src={[
-              "/lsd/pro/hero-columns/01.png",
-              "/lsd/pro/hero-columns/02.png",
-              "/lsd/pro/hero-columns/03.png",
-              "/lsd/pro/hero-columns/04.png",
-              "/lsd/pro/hero-columns/05.png",
-              "/lsd/pro/hero-columns/06.png",
-            ]}
-          />
-        }
-      >
-        <>
-          <h1>LSD Closed Beta</h1>
-          <p>
-            Welcome to LSD Closed Beta. Please sign in with Google to continue.
-          </p>
-          <ContinueWithGoogleButton />
-        </>
-      </UpgradeModalLayout>
+
+      <UpgradeToProDialog />
     </main>
+  );
+}
+
+function SigninGate() {
+  <UpgradeToProSplashView
+    hero={
+      <ColumnImages
+        src={[
+          "/lsd/pro/hero-columns/01.png",
+          "/lsd/pro/hero-columns/02.png",
+          "/lsd/pro/hero-columns/03.png",
+          "/lsd/pro/hero-columns/04.png",
+          "/lsd/pro/hero-columns/05.png",
+          "/lsd/pro/hero-columns/06.png",
+        ]}
+      />
+    }
+  >
+    <>
+      <h1>LSD Closed Beta</h1>
+      <p>Welcome to LSD Closed Beta. Please sign in with Google to continue.</p>
+      <ContinueWithGoogleButton />
+    </>
+  </UpgradeToProSplashView>;
+}
+
+function UpgradeToProDialog() {
+  const [view, setView] = React.useState<"splash" | "plans">("splash");
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className="Button violet">Upgrade</button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="DialogOverlay" />
+        <Dialog.Content className="DialogContent">
+          {view === "plans" ? (
+            <>
+              <UpgradeToProPlansView />
+            </>
+          ) : (
+            <>
+              <UpgradeToProSplashView
+                hero={
+                  <ColumnImages
+                    src={[
+                      "/lsd/pro/hero-columns/01.png",
+                      "/lsd/pro/hero-columns/02.png",
+                      "/lsd/pro/hero-columns/03.png",
+                      "/lsd/pro/hero-columns/04.png",
+                      "/lsd/pro/hero-columns/05.png",
+                      "/lsd/pro/hero-columns/06.png",
+                    ]}
+                  />
+                }
+              >
+                <>
+                  <h1>Upgrade to Pro</h1>
+                  <p>
+                    You’ve reached daily quota limit for Free tier limit.
+                    Upgrade to Pro and get access to exclusive templates and
+                    xxx.
+                  </p>
+                  <button
+                    onClick={() => {
+                      setView("plans");
+                    }}
+                  >
+                    View Plans
+                  </button>
+                </>
+              </UpgradeToProSplashView>
+            </>
+          )}
+
+          <Dialog.Close asChild>
+            <button className="IconButton" aria-label="Close">
+              <Cross2Icon />
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
