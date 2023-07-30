9eaa93709f9f77cce5dc7bf2406f0422c738f6a1
softmarshmallow <woojoo@softmarshmallow.com>
2023-07-30 18:18:14 +0900

    update view plans modal

--- web/scaffold/upgrade/upgrade-to-pro-plans-view-modal.tsx
+++ web/scaffold/upgrade/upgrade-to-pro-plans-view-modal.tsx
@@ -1,6 +1,10 @@
 import { PricingCard } from "components/pricing";
+import styled from "@emotion/styled";
 import plans from "k/plans.json";
 import contacts from "k/contacts.json";
+import faqs from "k/faq.json";
+import { motion } from "framer-motion";
+import { FaqItem } from "@/components/faq";
 
 const price_size = {
   normal: { width: 220 } as const,
@@ -9,8 +13,11 @@ const price_size = {
 
 export function UpgradeToProPlansView() {
   return (
-    <>
-      <div>
+    <PlansViewWrapper>
+      <motion.h1 initial={{ opacity: 0, y: 12 }} animate={{ opacity: 1, y: 0 }}>
+        Upgrade to Pro
+      </motion.h1>
+      <motion.section className="pricing-table">
         <PricingCard
           style={price_size.normal}
           plan="Personal"
@@ -76,7 +83,54 @@ export function UpgradeToProPlansView() {
             </button>
           }
         />
-      </div>
-    </>
+      </motion.section>
+      <motion.hr
+        initial={{ opacity: 0, y: 12 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+      />
+      <motion.section
+        className="faq section"
+        initial={{ opacity: 0, y: 12 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.6 }}
+      >
+        <motion.h2
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+        >
+          FAQ
+        </motion.h2>
+        <motion.div
+          className="list"
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+        >
+          {faqs.map((_, i) => (
+            <FaqItem key={i} {..._} />
+          ))}
+        </motion.div>
+      </motion.section>
+    </PlansViewWrapper>
   );
 }
+
+const PlansViewWrapper = styled.div`
+  font-family: "Inter", sans-serif;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .pricing-table {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    margin-top: 32px;
+  }
+`;
