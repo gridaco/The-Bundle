8a6c43bbeca3e95cc716274db7ed201c6d060a54
softmarshmallow <woojoo@softmarshmallow.com>
2023-08-01 13:48:16 +0900

    update state

--- web/core/states/editor-state.ts
+++ web/core/states/editor-state.ts
@@ -1,6 +1,14 @@
+type Plan = "anon" | "free" | "pro" | "team";
+
 export interface EditorState {
   templateId: string;
   templatePreset: string;
   templatePresets: string[];
   templateData: any;
+  plan: Plan;
+  ratelimit: {
+    limit: number;
+    remaining: number;
+    reset: number;
+  };
 }
