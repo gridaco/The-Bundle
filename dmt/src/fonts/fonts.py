aebf40a010832e1a02a430aaad1c22c91c1a1e1a
softmarshmallow <woojoo@softmarshmallow.com>
2023-08-10 18:57:11 +0900

    init google fonts repo

--- dmt/src/fonts/fonts.py
+++ dmt/src/fonts/fonts.py
@@ -1,22 +1,7 @@
-import subprocess
-import re
 
-
-def load_google_fonts():
-    """
-    Download the google fonts via
-    `git clone https://github.com/google/fonts.git`
-    """
-    subprocess.run(['git', 'clone', 'https://github.com/google/fonts.git'])
-
-
-def fallback(text):
-    """
-    Fallback font
-    """
-    # if Korean - [ㄱ-ㅎ|ㅏ-ㅣ|가-힣] - use Noto Sans KR
-    contains_korean = re.search(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]', text)
-    if contains_korean:
-        return 'NotoSansKR-Black.otf'
-
-    return False
+def font(familly,
+         weight='regular',
+         italic=False,
+         fallback=True,
+         ):
+    ...
---
+++ dmt/src/fonts/google_fonts.py
@@ -0,0 +1,68 @@
+import os
+from pathlib import Path
+from fontTools.ttLib import TTFont
+from dmt.src.fonts.axes import get_font_axes
+
+__dir = os.path.dirname(os.path.realpath(__file__))
+font_repo = os.path.join(__dir, '..', '..', '..', 'fonts')
+
+
+class GoogleFontsRepository:
+    repo: Path
+    fonts_path_map: dict = {}
+
+    def __init__(self, repo=font_repo):
+        self.repo = Path(repo)
+
+        # init the fonts directories - these are the directories that contain the fonts
+        licenses = ['apache', 'ofl', 'ufl']
+        for license in licenses:
+            # list all the directories under the license
+            license_dir = self.repo / license
+            for font_dir in os.listdir(license_dir):
+                name = Path(font_dir).name
+                self.fonts_path_map[name] = f'{license}/{font_dir}'
+
+    def family(self, name):
+        """
+        finds the font family by name
+        while searching, it will
+        flatten the font family name, e.g. Noto Sans KR -> notosanskr
+        """
+        name = name.lower().replace(' ', '')
+        return self.fonts_path_map[name]
+
+    def font(self, family, weight):
+        """
+        finds the font by family and weight
+        """
+        path = self.repo / self.family(family)
+
+        # Convert the weight string to integer for comparison
+        weight_int = int(weight)
+
+        # Iterate through all font files in the directory
+        for font_file in path.glob('*.ttf'):
+            ttfont = TTFont(font_file)
+            axes = get_font_axes(ttfont)
+            if axes:
+                # this is a variable font
+                # find the weight axis
+                weight_axis = next(
+                    filter(lambda axis: axis['tag'] == 'wght', axes))
+                if weight_axis['min'] <= weight_int <= weight_axis['max']:
+                    # the weight is within the range
+                    # load the font
+                    return font_file
+            else:
+                ...
+
+
+if __name__ == '__main__':
+    repo = GoogleFontsRepository()
+    # f = repo.font('Noto Sans KR', 400)
+    # print(f)
+    f2 = repo.font('OI', 400)
+    print(f2)
+    f3 = repo.font('Cairo', 400)
+    print(f3)
