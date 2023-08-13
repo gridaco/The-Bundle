59257f271faa296dc822d1747a340c8240bfff58
softmarshmallow <woojoo@softmarshmallow.com>
2023-08-14 00:28:54 +0900

    add smooth image transition

--- web/scaffolds/editor/canvas.tsx
+++ web/scaffolds/editor/canvas.tsx
@@ -5,38 +5,69 @@ import { BakedImageSequence3DView } from "components/interactive-3d-object-baked
 import { useEditor } from "@/core/states/use-editor";
 import { motion } from "framer-motion";
 
+function IdleOverlay({ idle }: { idle?: boolean }) {
+  return (
+    <motion.div
+      className="loading"
+      variants={{
+        hide: {
+          opacity: 0,
+        },
+        show: {
+          opacity: 1,
+        },
+      }}
+      animate={idle ? "show" : "hide"}
+    >
+      {idle && (
+        <LinearProgress
+          sx={{
+            backgroundColor: "white",
+            "& .MuiLinearProgress-bar": {
+              backgroundColor: "black",
+            },
+          }}
+          style={{
+            width: "50%",
+          }}
+        />
+      )}
+    </motion.div>
+  );
+}
+
 export function Canvas({ busy }: { busy?: boolean }) {
   const { template, result } = useEditor();
+  const [idle, setIdle] = useState(busy);
+  const [loaded, setLoaded] = useState(false);
 
   const src = result?.src ?? template.preview;
 
+  useEffect(() => {
+    if (busy) {
+      setLoaded(false);
+      setIdle(true);
+    }
+  }, [busy]);
+
+  useEffect(() => {
+    setLoaded(false);
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      setLoaded(true);
+    };
+  }, [src]);
+
+  useEffect(() => {
+    if (loaded) {
+      setIdle(false);
+    }
+  }, [loaded]);
+
   return (
     <CanvasWrapper>
-      {busy && (
-        <motion.div
-          className="loading"
-          initial={{
-            backdropFilter: "blur(0px)",
-            opacity: 0,
-          }}
-          animate={{
-            backdropFilter: "blur(32px)",
-            opacity: 1,
-          }}
-        >
-          <LinearProgress
-            sx={{
-              backgroundColor: "white",
-              "& .MuiLinearProgress-bar": {
-                backgroundColor: "black",
-              },
-            }}
-            style={{
-              width: "50%",
-            }}
-          />
-        </motion.div>
-      )}
+      <IdleOverlay idle={idle} />
       {/* <BakedImageSequence3DView
         style={{
           width: "100%",
@@ -48,11 +79,25 @@ export function Canvas({ busy }: { busy?: boolean }) {
         }}
       /> */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
+      <motion.img
         className="main"
         src={src}
+        variants={{
+          loaded: {
+            filter: "blur(0px)",
+            opacity: 1,
+          },
+          loading: {
+            filter: "blur(32px)",
+            opacity: 0.5,
+          },
+        }}
+        animate={loaded ? "loaded" : "loading"}
         onLoad={() => {
-          //
+          setTimeout(() => {
+            // 200ms delay to prevent flickering
+            setLoaded(true);
+          }, 200);
         }}
         alt="result"
       />
@@ -82,8 +127,8 @@ const CanvasWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background: rgba(0, 0, 0, 0.3);
-    z-index: 1;
+    background: rgba(0, 0, 0, 0.2);
+    z-index: 2;
   }
 
   /* disable right-click */
