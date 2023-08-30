b715e5ec567097e75afd301b1fd75037c2ca73fa
softmarshmallow <woojoo@softmarshmallow.com>
2023-08-30 13:55:53 +0900

    init layout

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col justify-between p-24">
       <section>
         <h1 className="text-9xl">The Bundle</h1>
         <button>Get The Bundle</button>
@@ -20,19 +20,27 @@ export default function Home() {
         <div className="bg-gray-700 flex-1" />
         <div className="bg-gray-900 flex-1" />
       </section>
-      <section>
-        <h2 className="text-3xl">
-          100,000 <span className="opacity-50">4K PNGs</span>
-          <br />
-          <span className="opacity-50">50 Angles</span> 500 Objects
-          <br />
-          50 Materials <span className="opacity-50">20 Scenes</span>
-        </h2>
-        <span>*Starting from $499 / Mo</span>
-        <div>
-          <button>Get The Bundle</button>
-          <button>Download Free Demo File</button>
+      <section className="flex">
+        <div className="flex flex-col">
+          <h2 className="text-3xl">
+            100,000 <span className="opacity-50">4K PNGs</span>
+            <br />
+            <span className="opacity-50">50 Angles</span> 500 Objects
+            <br />
+            50 Materials <span className="opacity-50">20 Scenes</span>
+          </h2>
+          <span>*Starting from $499 / Mo</span>
+          <div>
+            <button>Get The Bundle</button>
+            <button>Download Free Demo File</button>
+          </div>
         </div>
+        <Image
+          src="/cta-bg-4k-example.png"
+          alt={"cta-bg-4k-example"}
+          width={950}
+          height={750}
+        />
       </section>
       <footer>
         <span className="opacity-50">
