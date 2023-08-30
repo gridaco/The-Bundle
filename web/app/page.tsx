a3a527c2159efb020144ae593c4e1bfa7a4444e5
softmarshmallow <woojoo@softmarshmallow.com>
2023-08-30 14:06:26 +0900

    layout

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <section className="flex flex-row items-center">
+    <main className="flex min-h-screen flex-col justify-between gap-40">
+      <section className="flex flex-row items-center pl-24">
         <div className="flex flex-col items-start">
           <h1 className="text-9xl">The Bundle</h1>
           <button>Get The Bundle</button>
@@ -16,10 +16,9 @@ export default function Home() {
         />
       </section>
       <section
-        className="flex"
+        className="flex min-w-max p-20"
         style={{
-          width: 1500,
-          height: 500,
+          height: 900,
         }}
       >
         <div className="bg-gray-900 flex-1" />
@@ -27,8 +26,9 @@ export default function Home() {
         <div className="bg-gray-900 flex-1" />
         <div className="bg-gray-700 flex-1" />
         <div className="bg-gray-900 flex-1" />
+        <div className="bg-gray-700 flex-1" />
       </section>
-      <section className="flex">
+      <section className="flex pl-24 justify-between">
         <div className="flex flex-col">
           <h2 className="text-3xl">
             100,000 <span className="opacity-50">4K PNGs</span>
@@ -50,7 +50,7 @@ export default function Home() {
           height={750}
         />
       </section>
-      <footer>
+      <footer className="p-24">
         <span className="opacity-50">
           The Bundle by Grida - © {new Date().getFullYear()} Grida, Inc. All
           Rights Reserved.
