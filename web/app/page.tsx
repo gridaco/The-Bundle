346064fe62ab09cf984b1883c533a361202957a6
softmarshmallow <woojoo@softmarshmallow.com>
2023-08-30 15:16:31 +0900

    placements

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -9,7 +9,7 @@ const delta_gothic_one = Dela_Gothic_One({
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between gap-40">
+    <main className="relative flex min-h-screen flex-col justify-between gap-40">
       <section className="flex flex-row items-center justify-between pl-24">
         <div className="flex flex-col items-start">
           <h1 className="text-8xl">
@@ -28,19 +28,19 @@ export default function Home() {
         />
       </section>
       <section
-        className="flex min-w-max p-20"
+        className="flex min-w-max p-20 gap-2"
         style={{
           height: 900,
         }}
       >
-        <div className="bg-gray-900 flex-1" />
-        <div className="bg-gray-700 flex-1" />
-        <div className="bg-gray-900 flex-1" />
-        <div className="bg-gray-700 flex-1" />
-        <div className="bg-gray-900 flex-1" />
-        <div className="bg-gray-700 flex-1" />
+        <div className="bg-neutral-900 flex-1 rounded-sm" />
+        <div className="bg-neutral-700 flex-1 rounded-sm" />
+        <div className="bg-neutral-900 flex-1 rounded-sm" />
+        <div className="bg-neutral-700 flex-1 rounded-sm" />
+        <div className="bg-neutral-900 flex-1 rounded-sm" />
+        <div className="bg-neutral-700 flex-1 rounded-sm" />
       </section>
-      <section className="flex pl-24 justify-between">
+      <section className="flex pl-24 justify-between items-center">
         <div className="flex flex-col">
           <h2 className="text-3xl font-bold">
             100,000 <span className="opacity-50">4K PNGs</span>
@@ -52,10 +52,10 @@ export default function Home() {
           <span className="opacity-50 text-sm">*Starting from $499 / Mo</span>
           <div className="h-10" />
           <div className="flex gap-4">
-            <button className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-4 rounded">
+            <button className="bg-white hover:bg-neutral-200 text-black font-bold py-2 px-4 rounded">
               Get The Bundle
             </button>
-            <button className="border-white hover:bg-gray-900 text-white font-bold py-2 px-4 rounded">
+            <button className="border-white hover:bg-neutral-900 text-white font-bold py-2 px-4 rounded">
               Download Free Demo File
             </button>
           </div>
@@ -67,7 +67,7 @@ export default function Home() {
           height={750}
         />
       </section>
-      <footer className="p-24">
+      <footer className="pl-24 absolute bottom-8">
         <span className="opacity-50">
           The Bundle by Grida - © {new Date().getFullYear()} Grida, Inc. All
           Rights Reserved.
