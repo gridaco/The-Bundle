8017232835125f4c89e7be3919eef304bc792428
softmarshmallow <woojoo@softmarshmallow.com>
2023-09-04 16:39:10 +0900

    symlinks & json with comments

--- packager/main.py
+++ packager/main.py
@@ -13,6 +13,14 @@ logging.basicConfig(level=logging.DEBUG)
 __DIR = Path(__file__).parent
 
 
+def load_json_with_comments(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        lines = [line for line in lines if not line.strip().startswith('//')]
+        clean_data = ''.join(lines)
+    return json.loads(clean_data)
+
+
 class PackagingProfile:
     data: dict
     packaging: dict
@@ -23,7 +31,7 @@ class PackagingProfile:
     files: [str]
 
     def __init__(self, path: Path):
-        self.data = json.load(path.open("r"))
+        self.data = load_json_with_comments(str(path))
         self.pattern_i = self.data.get("pattern").get("i")
         self.pattern_o = self.data.get("pattern").get("o")
         self.packaging = self.data.get("packaging")
@@ -32,7 +40,7 @@ class PackagingProfile:
         self.files = self.data.get("files")
 
 
-def render_attatchments(pack: Path, attatchments: [str], target: str):
+def render_attatchments(pack: Path, attatchments: [str], target: str, prefix="_"):
     for att in attatchments:
         # resolve it
         # 1. check if the specified path is a file
@@ -43,13 +51,14 @@ def render_attatchments(pack: Path, attatchments: [str], target: str):
         if att.exists():
             # check if file
             if att.is_file():
-                shutil.copyfile(str(att), pack / att.name)
+                shutil.copyfile(str(att), pack / f"{prefix}{att.name}")
 
             # if a directory, resolve the file with "target", the name would be the parent name
             if att.is_dir():
                 for item in att.iterdir():
                     if item.stem == target:
-                        shutil.copy2(item, pack / item.parent.name)
+                        shutil.copy2(
+                            item, pack / f"{prefix}{item.parent.name}")
 
 
 def apply_mapping(mapping, value):
@@ -59,7 +68,7 @@ def apply_mapping(mapping, value):
     return None
 
 
-def render_mappings(path: Path, out: Path, objects_mapping: dict, materials_mapping: dict, symlink=False):
+def render_mappings(path: Path, out: Path, objects_mapping: dict, materials_mapping: dict, symlink=True):
     for material_dir in path.iterdir():
         if not material_dir.is_dir():
             continue
@@ -87,33 +96,42 @@ def render_mappings(path: Path, out: Path, objects_mapping: dict, materials_mapp
             object_out_path = material_out_path / mapped_object
             object_out_path.mkdir(parents=True, exist_ok=True)
 
+            dst = object_out_path / file.name
+
             if symlink:
                 # Create a symlink to the file
-                (file.resolve()).symlink_to(object_out_path / file.name)
+                dst.symlink_to(file)
             else:
                 # Copy the file to the new location
-                shutil.copy(file, object_out_path / file.name)
+                shutil.copy(file, dst)
 
 
 @click.command()
 @click.argument("path", type=click.Path(exists=True, file_okay=False))
-@click.option("-o", "--out", type=click.Path(file_okay=False), help="Path to output the package to")
+@click.option("-o", "--out", type=click.Path(exists=False, file_okay=False), help="Path to output the package to")
 @click.option("-p", "--profile", type=click.Path(exists=True, file_okay=True), help="Path to a profile to use (.json)")
 @click.option("--allow-unknown", is_flag=True, help="Allow unknown files to be packaged")
 @click.option("--package-master-file", is_flag=True, help="Package the master file (takes another double storage)")
-@click.option("--symlink", is_flag=True, help="Use symlink istead of actually copying files")
-def main(path, out, profile, allow_unknown, package_master_file, symlink):
+@click.option("--no-symlink", is_flag=True, help="Don't Use symlink. always copy")
+def main(path, out, profile, allow_unknown, package_master_file, no_symlink):
     path = Path(path)
 
     out = Path(out)
-    out.mkdir(exist_ok=True, parents=True)
+    # assert out dir is empty
+    assert not out.exists(), "Output directory must not exist"
+
+    out.mkdir(exist_ok=False, parents=True)
 
     assert path != out, "Path and out cannot be the same"
 
     profile = PackagingProfile(Path(profile))
 
     render_mappings(
-        path, out, profile.mapping["objects"], profile.mapping["materials"])
+        path, out,
+        profile.mapping["objects"],
+        profile.mapping["materials"],
+        symlink=not no_symlink
+    )
 
     # all output packs are double nested directories
     packs = []
@@ -123,8 +141,14 @@ def main(path, out, profile, allow_unknown, package_master_file, symlink):
                 if subsubdir.is_dir():
                     packs.append(subsubdir)
 
-    for pack in packs:
+    # include attatchments
+    for pack in tqdm(packs, desc="Rendering Attatchments"):
         render_attatchments(pack, profile.files, 'pro')
+
+    # archive the packs
+    for pack in tqdm(packs, desc="Archiving individual packs"):
+        # this will automatically resolve all symlinks
+        shutil.make_archive(str(pack), 'zip', str(pack))
 
 
 if __name__ == "__main__":
