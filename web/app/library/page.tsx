aa3fcdb052863be13e3843579beaae125e6710ed
softmarshmallow <woojoo@softmarshmallow.com>
2023-09-05 12:53:45 +0900

    add guard

--- web/app/library/page.tsx
+++ web/app/library/page.tsx
@@ -1,6 +1,19 @@
 import Link from "next/link";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { app_metadata_subscription_id } from "@/k/userkey.json";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LibraryPage() {
+export default async function LibraryPage() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const { data } = await supabase.auth.getUser();
+  const pro = data.user?.app_metadata[app_metadata_subscription_id];
+
+  if (!pro) {
+    redirect("/bundle");
+  }
+
   return (
     <>
       <Link
