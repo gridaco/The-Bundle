01d7f7032f3d52d71492af7d292511623bfb9ca0
softmarshmallow <woojoo@softmarshmallow.com>
2023-09-05 12:17:25 +0900

    update download

--- web/app/library/download/route.ts
+++ web/app/library/download/route.ts
@@ -2,7 +2,13 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { app_metadata_subscription_id } from "@/k/userkey.json";
+import { generateSignedDownloadLink } from "@/s/stripe/s3";
 
 export async function GET(request: NextRequest) {
   // sign s3 url if user is signed in as a pro user
+  const bucket = "the-bundle-dist";
+  const key = "v1/bin/cmp.gold-foil/99.zip"; // TODO: replace this
+  const signedUrl = await generateSignedDownloadLink(bucket, key);
+
+  return NextResponse.redirect(signedUrl);
 }
--- web/app/library/page.tsx
+++ web/app/library/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function LibraryPage() {
-  return <></>;
+  return (
+    <>
+      <Link
+        href="/library/download?item=m.copper-a.zip"
+        download
+        target="_blank"
+      >
+        <button>Download Copper-A</button>
+      </Link>
+    </>
+  );
 }
