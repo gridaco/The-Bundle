f99f5bc5471331572623c25f98018c5a23e7587b
softmarshmallow <woojoo@softmarshmallow.com>
2023-09-13 14:55:26 +0900

    force dark

--- web/app/welcome/page.tsx
+++ web/app/welcome/page.tsx
@@ -49,7 +49,7 @@ export default async function WelcomePage() {
             id="job-title"
             name="job-title"
             type="text"
-            className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="text-sm rounded-lg block w-full p-2.5 dark:bg-neutral-700 border-neutral-600 placeholder-neutral-400 text-white focus:ring-blue-500 focus:border-blue-500"
             placeholder="Graphics Designer"
             required
             autoComplete="off"
@@ -67,7 +67,7 @@ export default async function WelcomePage() {
             id="team-size"
             name="team-size"
             type="text"
-            className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="text-sm rounded-lg block w-full p-2.5 dark:bg-neutral-700 border-neutral-600 placeholder-neutral-400 text-white focus:ring-blue-500 focus:border-blue-500"
             placeholder="1 ~ 3"
             required
             autoComplete="off"
@@ -85,7 +85,7 @@ export default async function WelcomePage() {
             id="instagram"
             name="instagram"
             type="text"
-            className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="text-sm rounded-lg block w-full p-2.5 dark:bg-neutral-700 border-neutral-600 placeholder-neutral-400 text-white focus:ring-blue-500 focus:border-blue-500"
             placeholder="@grida.co"
             required
             autoComplete="off"
