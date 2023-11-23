02d125d36ab88b4fd418829c58d70f0056f1b307
Universe <universe@grida.co>
2023-11-23 21:29:16 +0900

    header with authstate

--- web/app/(library)/page.tsx
+++ web/app/(library)/page.tsx
@@ -1,53 +1,22 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+
 import { Packs, MaterialsNav } from "@/library";
-import { isProUser } from "@/s/q-user";
-import { DemoDownloadCard } from "@/components/demo-download-card";
+
 import { ScrollToTop } from "@/components/scroll-to-top";
 import Footer from "@/library/footer";
 import { ScrollArea } from "@radix-ui/themes";
+import { DemoDownloadCardIfNotPro } from "@/library/demo";
 
 export const dynamic = "force-dynamic";
 
-export default async function LibraryPage() {
-  const cookieStore = cookies()
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-      },
-    }
-  )
-
-  const { data } = await supabase.auth.getUser();
-
-  if (!data.user) {
-    redirect("/bundle/signin");
-  }
-
-  const pro = data.user && isProUser(data.user);
+export default function LibraryPage() {
 
   return (
     <Layout>
       <main className="p-10 overflow-scroll">
-        {!pro && (
-          <div className="max-w-screen-md m-auto mt-24 mb-24 border border-opacity-10 border-white p-8 rounded-sm hover:border-opacity-20">
-            <DemoDownloadCard />
-          </div>
-        )}
         <div className="mt-24">
           <Packs />
         </div>
-        {!pro && (
-          <div className="max-w-screen-md m-auto mt-24 mb-24 border border-opacity-10 border-white p-8 rounded-sm hover:border-opacity-20">
-            <DemoDownloadCard />
-          </div>
-        )}
+        <DemoDownloadCardIfNotPro />
         {/* scroll to top */}
         <div className="m-auto w-fit mt-40">
           <ScrollToTop />
--- web/components/header/actions.tsx
+++ web/components/header/actions.tsx
@@ -3,18 +3,43 @@
 import React from 'react';
 import Link from "next/link";
 import { Button } from "@radix-ui/themes";
-
+import { useUser } from '@/hooks/useUser';
 
 export function Actions() {
+  const user = useUser();
 
-  return <div className="flex gap-4">
+
+  const _subscribe = (
     <Button className="bg-white text-black rounded-md p-4 font-medium">
       <Link href="/library">
         Subscribe
       </Link>
     </Button>
+  )
+
+  if (user) {
+    return (
+      <div className="flex gap-4">
+        {
+          !user.isPro && <>{_subscribe}</>
+        }
+        <Button className="bg-black text-white rounded-md p-4 font-medium">
+          <Link href="/downloads">
+            Downloads
+          </Link>
+        </Button>
+      </div>
+    )
+  }
+
+  return <div className="flex gap-4">
+    <Button className="bg-white text-black rounded-md p-4 font-medium">
+      <Link href="/pro">
+        Subscribe
+      </Link>
+    </Button>
     <Button className="bg-black text-white rounded-md p-4 font-medium">
-      <Link href="/library">
+      <Link href="/signin">
         Sign In
       </Link>
     </Button>
